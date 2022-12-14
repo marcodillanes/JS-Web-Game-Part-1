68643b715f3f969aa23ee8bfebfdeb31f384c682
Marco <114459798+marcodillanes@users.noreply.github.com>
2022-12-13 17:23:23 -0800

    changed code twice

--- index.js
+++ index.js
@@ -1,6 +1,66 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+// rewrite existing code using a function at the top of index.js named newImage
+// function newImage () {
+//     let greenCharacter = document.createElement('img')
+//     greenCharacter.src = 'assets/green-character.gif'
+//     greenCharacter.style.position = 'fixed'
+//     greenCharacter.style.left = '100px'
+//     greenCharacter.style.bottom = '100px'
+//     document.body.append(greenCharacter)
+// }
+//  newImage ()
+
+
+// rewrite code above to allow function and newImage to display assets!
+
+function newImage(url, left, bottom){
+    let object = document.createElement('img')
+    object.src = url
+    object.style.position = 'fixed'
+    object.style.left = left + 'px'
+    object.style.bottom = bottom + 'px'
+    document.body.append(object)
+    return object
+}
+
+function newItem(url, left, bottom){
+    let object = newImage(url, left, bottom)
+    object.addEventListener('dblclick', () => {
+        object.remove()
+    })
+
+
+}
+
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+
+newItem('assets/sword.png', 500, 405)
+newItem('assets/shield.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
+
+
+
+
+
+
+
+// let greenCharacter = document.createElement('img')
+// greenCharacter.src = 'assets/green-character.gif'
+// greenCharacter.style.position = 'fixed'
+// greenCharacter.style.left = '100px'
+// greenCharacter.style.bottom = '100px'
+// document.body.append(greenCharacter)
+
+// duplicate code to add a pine tree 450px to the left and 200px from the bottom
+
+// let pineTree = document.createElement('img')
+// pineTree.src = 'assets/pine-tree.png'
+// pineTree.style.position = 'fixed'
+// pineTree.style.left = '450px'
+// pineTree.style.bottom = '200px'
+// document.body.append(pineTree)
+
